cc836e9c3e75715b2abb577ad168e390cd827cee
Md Fahim <fahim10839@gmail.com>
2022-11-27 23:45:16 +0530

    chnages on prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  shopperUrl: "https://localhost:8443/api",
+  stripePublishableKey: "pk_test_51LHmSRSC2IowzRujblaOs3jCfGzivJZt6ACvHnA6ZgL3N5cbGjwbs2c9OHOfNnuu88skfNSZKhxruyYLLIUJpOQQ00lVERwEWk"
 };
